15de723728165e680267aecc5b973eb4297c7ce6
Erdenezaya Batnasan <erdenezaya.b@nmtec.co>
2025-04-09 13:20:11 +0800

    retain only home page in the route

--- habit_tracker/app/routes.py
+++ habit_tracker/app/routes.py
@@ -4,7 +4,3 @@ app = Flask(__name__)
 @app.rout('/')
 def home():
     return "Welcome to the Habit Tracker!"
-@app.route('/habits')
-def habits():
-    return "Here are your habits."
-@app.route('/habits/<int:habit_id>')
